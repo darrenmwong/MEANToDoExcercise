3618325109bf9605d7f7030e8f1919af2709dd58
darrenmwong <darrenwong06@gmail.com>
2014-09-04 16:14:43 -0700

    Defines Todo Model

--- server.js
+++ server.js
@@ -20,3 +20,8 @@
   // listen (start app with node server.js) ======================================
   app.listen(8080);
   console.log("App listening on port 8080");
+
+  // define model =================
+  var Todo = mongoose.model('Todo', {
+    text : String
+  });
